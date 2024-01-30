bd2f51950c08d7070b29d524ba8bc551d0b1f065
franciscofox <101645073+franciscofox@users.noreply.github.com>
2024-01-30 10:34:34 -0300

    Removed passwords from code

--- database.js
+++ database.js
@@ -1,30 +1,10 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('schooldb', 'postgres', 'pass', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: 'localhost',
     dialect: 'postgres'
 });
 
 module.exports = sequelize;
 
-
-// const { Sequelize } = require('sequelize');
-
-// const dbName = 'schooldb';
-// const dbUser = 'postgres'; 
-// const dbPass = 'password'; 
-// const dbHost = 'schooldb.czyga4gsq172.us-east-1.rds.amazonaws.com';
-
-// const sequelize = new Sequelize(dbName, dbUser, dbPass, {
-//     host: dbHost,
-//     dialect: 'postgres',
-//     port: 5432,
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
-
-// module.exports = sequelize;
